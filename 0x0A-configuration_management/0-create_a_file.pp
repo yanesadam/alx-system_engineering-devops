118ebd010cbe888f299b7d51b3917293bcb56269
Yanes Adam <34406127+yanesadam@users.noreply.github.com>
2022-09-06 06:01:47 +0100

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a file
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
